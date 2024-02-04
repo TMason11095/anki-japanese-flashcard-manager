7827df0a3e4e5e0995b62aed41a2ec7c55311a1f
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-02-04 10:08:50 -0600

    Implemented unit tests for Anki2Controller.GetTaggedDecks().

--- AnkiJapaneseFlashcardManagerTest/Anki2ControllerTests.cs
+++ AnkiJapaneseFlashcardManagerTest/Anki2ControllerTests.cs
@@ -1,11 +1,41 @@
+using AnkiSentenceCardBuilder.Controllers;
+using FluentAssertions;
+
 namespace AnkiJapaneseFlashcardManagerTest
 {
 	public class Anki2ControllerTests
 	{
-		[Theory]
-		public void Get_decks_by_description_tag()
-		{
+		string _anki2FolderPath = "./Resources/Anki2 Files/";
 
+		[Theory]
+		[InlineData("empty_kanjiResource_deck.anki2", "KanjiResource")]
+		[InlineData("empty_random_decks.anki2", "Random")]
+		[InlineData("empty_random_decks.anki2", "RandomResource")]
+		public void Get_decks_by_description_tag(string anki2File, string deckTagName)
+		{
+			//Arange
+			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
+
+			//Act
+			var taggedDecks = anki2Controller.GetTaggedDecks(deckTagName);
+
+			//Assert
+			taggedDecks.Should().NotBeEmpty();
+		}
+
+		[Theory]
+		[InlineData("empty_kanjiResource_deck.anki2", "NonExistentTag")]
+		[InlineData("empty_random_decks.anki2", "KanjiResource")]
+		public void No_decks_with_matching_description_tag_is_empty(string anki2File, string deckTagName)
+		{
+			//Arange
+			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
+
+			//Act
+			var taggedDecks = anki2Controller.GetTaggedDecks(deckTagName);
+
+			//Assert
+			taggedDecks.Should().BeEmpty();
 		}
 	}
 }
