bceb62eb52d2ef77457993ae13ca15696bbecea8
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-02-04 10:16:21 -0600

    Implemented unit tests for Anki2Controller.GetResourceKanjiDecks().

--- AnkiJapaneseFlashcardManagerTest/Anki2ControllerTests.cs
+++ AnkiJapaneseFlashcardManagerTest/Anki2ControllerTests.cs
@@ -37,5 +37,33 @@ namespace AnkiJapaneseFlashcardManagerTest
 			//Assert
 			taggedDecks.Should().BeEmpty();
 		}
+
+		[Theory]
+		[InlineData("empty_kanjiResource_deck.anki2")]
+		public void Get_kanji_resource_decks(string anki2File)
+		{
+			//Arange
+			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
+
+			//Act
+			var taggedDecks = anki2Controller.GetResourceKanjiDecks();
+
+			//Assert
+			taggedDecks.Should().NotBeEmpty();
+		}
+
+		[Theory]
+		[InlineData("empty_random_decks.anki2")]
+		public void No_kanji_resource_decks_is_empty(string anki2File)
+		{
+			//Arange
+			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
+
+			//Act
+			var taggedDecks = anki2Controller.GetResourceKanjiDecks();
+
+			//Assert
+			taggedDecks.Should().BeEmpty();
+		}
 	}
 }
