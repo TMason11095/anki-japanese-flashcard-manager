21c90be214a73ef0d39795e16b981e930cbd44af
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-02-19 21:23:08 -0600

    Implemented MoveResourceSubKanjiToNewKanji().
    Unit test Move_resource_sub_kanji_notes_to_new_kanji_deck() passes by itself but fails when ran with all tests. The issue comes from the test Move_notes_between_decks() keeping the same test file open and preventing this test from refreshing the data.
    Issue marked in:
    https://github.com/TMason11095/anki-japanese-flashcard-manager/issues/6

--- AnkiSentenceCardBuilder/Controllers/Anki2Controller.cs
+++ AnkiSentenceCardBuilder/Controllers/Anki2Controller.cs
@@ -230,9 +230,28 @@ namespace AnkiSentenceCardBuilder.Controllers
 			return MoveNotesBetweenDecks(newKanjiNoteIdsToMove, learningKanjiDeckId);
 		}
 
-		public bool MoveResourceSubKanjiToNewKanji()//TODO
+		public bool MoveResourceSubKanjiToNewKanji()
 		{
-			return false;
+			//Get the kanji resource decks
+			var kanjiResourceDecks = GetResourceKanjiDecks();
+			//Get the kanji resource notes
+			var kanjiResourceNotes = kanjiResourceDecks.SelectMany(d => GetDeckNotes(d.Id)).ToList();
+			//Get the new kanji decks
+			var newKanjiDecks = GetNewKanjiDecks();
+			//Fail if no new kanji decks found
+			if (!newKanjiDecks.Any()) { return false; }
+			//Get the new kanji notes
+			var newKanjiNotes = newKanjiDecks.SelectMany(d => GetDeckNotes(d.Id)).ToList();
+			//Pull kanji resource notes based on the new kanji sub kanji ids
+			var SubKanjiResourceNotes = PullAllSubKanjiNotesFromNoteList(ref kanjiResourceNotes, newKanjiNotes);
+			//Skip if no new kanji sub kanji notes to move
+			if (!SubKanjiResourceNotes.Any()) { return true; }
+			//Get the sub kanji resource note ids
+			var subKanjiResourceNoteIdsToMove = SubKanjiResourceNotes.Select(n => n.Id).ToList();
+			//Get the first new kanji deck id to move the resource kanji notes to
+			var newKanjiDeckId = newKanjiDecks.First().Id;
+			//Move the resource kanji notes to the new kanji deck
+			return MoveNotesBetweenDecks(subKanjiResourceNoteIdsToMove, newKanjiDeckId);
 		}
 	}
 }
--- AnkiSentenceCardBuilder/Services/LocalTestingFunction.cs
+++ AnkiSentenceCardBuilder/Services/LocalTestingFunction.cs
@@ -36,14 +36,14 @@ namespace AnkiSentenceCardBuilder.Services
 			//List<Deck> decks = anki2Controller.GetTable<Deck>();
 
 			//Find the decks with the kanji resource binding
-			List<Deck> resourceKanjiDecks = anki2Controller.GetResourceKanjiDecks();
+				List<Deck> resourceKanjiDecks = anki2Controller.GetResourceKanjiDecks();
 			//Find the decks with the new kanji binding
 			List<Deck> newKanjiDecks = anki2Controller.GetNewKanjiDecks();
 			//Find the decks with the learning kanji binding
 			List<Deck> learningKanjiDecks = anki2Controller.GetLearningKanjiDecks();
 
 			//Get notes from the kanji resource decks
-			List<Note> resourceKanjiNotes = resourceKanjiDecks.SelectMany(d => anki2Controller.GetDeckNotes(d.Id)).ToList();
+				List<Note> resourceKanjiNotes = resourceKanjiDecks.SelectMany(d => anki2Controller.GetDeckNotes(d.Id)).ToList();
 			//Get notes from the new kanji decks
 			List<Note> newKanjiNotes = newKanjiDecks.SelectMany(d => anki2Controller.GetDeckNotes(d.Id)).ToList();
 
