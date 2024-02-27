a4243e7644499c6a6e5e4381c3c7ed0d42757754
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-02-27 07:10:53 -0600

    Moved unit tests related to GetLearningKanjiDecks function into KanjiDeckServiceTests.

--- AnkiJapaneseFlashcardManagerTest/Anki2DeckControllerTests.cs
+++ AnkiJapaneseFlashcardManagerTest/Anki2DeckControllerTests.cs
@@ -43,33 +43,5 @@ namespace AnkiJapaneseFlashcardManagerTests
 			//Assert
 			taggedDecks.Should().BeEmpty();
 		}
-
-		[Theory]
-		[InlineData("emptyLearningKanji_飲newKanji_食欠人良resourceKanji_decks.anki2")]
-		public void Get_learning_kanji_decks(string anki2File)//TODO: Refactor to check for expected values
-		{
-			//Arange
-			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
-
-			//Act
-			var learningKanjiDecks = anki2Controller.GetLearningKanjiDecks();
-
-			//Assert
-			learningKanjiDecks.Should().NotBeEmpty();
-		}
-
-		[Theory]
-		[InlineData("empty_random_decks.anki2")]
-		public void No_learning_kanji_decks_is_empty(string anki2File)
-		{
-			//Arange
-			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
-
-			//Act
-			var learningKanjiDecks = anki2Controller.GetLearningKanjiDecks();
-
-			//Assert
-			learningKanjiDecks.Should().BeEmpty();
-		}
 	}
 }
