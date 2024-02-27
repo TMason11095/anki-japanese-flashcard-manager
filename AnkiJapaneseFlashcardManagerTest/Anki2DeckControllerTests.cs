4eecd95feff3aefbbce6f441fd3e1e1c10d803ef
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-02-27 06:47:24 -0600

    Moved unit tests related to GetResourceKanjiDecks function into KanjiDeckServiceTests.

--- AnkiJapaneseFlashcardManagerTest/Anki2DeckControllerTests.cs
+++ AnkiJapaneseFlashcardManagerTest/Anki2DeckControllerTests.cs
@@ -45,35 +45,6 @@ namespace AnkiJapaneseFlashcardManagerTests
 		}
 
 		[Theory]
-		[InlineData("empty_kanjiResource_deck.anki2")]
-		[InlineData("empty_kanjiResource_newKanji_decks.anki2")]
-		public void Get_kanji_resource_decks(string anki2File)//TODO: Refactor to check for expected values
-		{
-			//Arange
-			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
-
-			//Act
-			var taggedDecks = anki2Controller.GetResourceKanjiDecks();
-
-			//Assert
-			taggedDecks.Should().NotBeEmpty();
-		}
-
-		[Theory]
-		[InlineData("empty_random_decks.anki2")]
-		public void No_kanji_resource_decks_is_empty(string anki2File)
-		{
-			//Arange
-			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
-
-			//Act
-			var taggedDecks = anki2Controller.GetResourceKanjiDecks();
-
-			//Assert
-			taggedDecks.Should().BeEmpty();
-		}
-
-		[Theory]
 		[InlineData("empty_newKanji_deck.anki2")]
 		[InlineData("empty_kanjiResource_newKanji_decks.anki2")]
 		public void Get_new_kanji_decks(string anki2File)//TODO: Refactor to check for expected values
---
+++ AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/KanjiDeckServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using AnkiSentenceCardBuilder.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnkiJapaneseFlashcardManagerTests.ApplicationLayer.Services
+{
+	public class KanjiDeckServiceTests
+	{
+		//TODO: MOVE TO GLOBAL VARIABLE
+		string _anki2FolderPath = "./Resources/Anki2 Files/";
+
+		[Theory]
+		[InlineData("empty_kanjiResource_deck.anki2")]
+		[InlineData("empty_kanjiResource_newKanji_decks.anki2")]
+		public void Get_kanji_resource_decks(string anki2File)//TODO: Refactor to check for expected values
+		{
+			//Arange
+			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
+
+			//Act
+			var taggedDecks = anki2Controller.GetResourceKanjiDecks();
+
+			//Assert
+			taggedDecks.Should().NotBeEmpty();
+		}
+
+		[Theory]
+		[InlineData("empty_random_decks.anki2")]
+		public void No_kanji_resource_decks_is_empty(string anki2File)
+		{
+			//Arange
+			Anki2Controller anki2Controller = new Anki2Controller(_anki2FolderPath + anki2File);
+
+			//Act
+			var taggedDecks = anki2Controller.GetResourceKanjiDecks();
+
+			//Assert
+			taggedDecks.Should().BeEmpty();
+		}
+	}
+}
