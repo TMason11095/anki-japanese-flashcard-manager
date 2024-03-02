5785c2cd73c59dd9d3c5cbfae98badb42e8bf523
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-02 11:17:17 -0600

    Moved AnkiBindingTagsDTO into the Application Layer.

--- AnkiSentenceCardBuilder/ApplicationLayer/Config/AnkiBindingConfig.cs
+++ AnkiSentenceCardBuilder/ApplicationLayer/Config/AnkiBindingConfig.cs
@@ -1,4 +1,4 @@
-﻿using AnkiJapaneseFlashcardManager.Models.DTO;
+﻿using AnkiJapaneseFlashcardManager.ApplicationLayer.DTOs.Config;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
---
+++ AnkiSentenceCardBuilder/ApplicationLayer/DTOs/Config/AnkiBindingTagsDTO.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnkiJapaneseFlashcardManager.ApplicationLayer.DTOs.Config
+{
+    public class AnkiBindingTagsDTO
+    {
+        [JsonProperty("deckTag")]
+        public string DeckTag { get; protected set; }
+        [JsonProperty("resourceDecks")]
+        public ResourceDecksDTO ResourceDecks { get; protected set; }
+        [JsonProperty("newDecks")]
+        public NewDecksDTO NewDecks { get; protected set; }
+        [JsonProperty("learningDecks")]
+        public LearningDecksDTO LearningDecks { get; protected set; }
+        [JsonProperty("noteTags")]
+        public NoteTagsDTO NoteTags { get; protected set; }
+        [JsonProperty("noteIntervalLimits")]
+        public NoteIntervalLimitsDTO NoteIntervalLimits { get; protected set; }
+    }
+
+    public class ResourceDecksDTO
+    {
+        [JsonProperty("kanji")]
+        public string Kanji { get; protected set; }
+    }
+
+    public class NewDecksDTO
+    {
+        [JsonProperty("kanji")]
+        public string Kanji { get; protected set; }
+    }
+
+    public class LearningDecksDTO
+    {
+        [JsonProperty("kanji")]
+        public string Kanji { get; protected set; }
+    }
+
+    public class NoteTagsDTO
+    {
+        [JsonProperty("kanjiId")]
+        public string KanjiId { get; protected set; }
+        [JsonProperty("subKanjiId")]
+        public string SubKanjiId { get; protected set; }
+    }
+
+    public class NoteIntervalLimitsDTO
+    {
+        [JsonProperty("moveFromNewKanji")]
+        public int MoveFromNewKanji { get; protected set; }
+    }
+}
