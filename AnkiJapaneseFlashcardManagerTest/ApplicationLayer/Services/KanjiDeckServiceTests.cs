a0cd54e567975256dc46315ab74a81ad63c20382
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-02 18:35:10 -0600

    Refactored No_kanji_resource_decks_is_empty to include expected values.

--- AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/KanjiDeckServiceTests.cs
+++ AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/KanjiDeckServiceTests.cs
@@ -32,8 +32,8 @@ namespace Tests.ApplicationLayer.Services
 		}
 
 		[Theory]
-		[InlineData("empty_random_decks.anki2")]
-		public void No_kanji_resource_decks_is_empty(string anki2File)
+		[InlineData("empty_random_decks.anki2", new long[0])]
+		public void No_kanji_resource_decks_is_empty(string anki2File, long[] expectedDeckIds)
 		{
 			//Arange
 			Anki2Context anki2Context = new Anki2Context(_anki2FolderPath + anki2File);
@@ -44,7 +44,7 @@ namespace Tests.ApplicationLayer.Services
 			var taggedDecks = kanjiDeckService.GetResourceKanjiDecks();
 
 			//Assert
-			taggedDecks.Should().BeEmpty();
+			taggedDecks.Select(d => d.Id).Should().BeEquivalentTo(expectedDeckIds);
 		}
 
 		[Theory]
