988077956570837e462a4be68ae305a83f8a57c3
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-02 18:43:40 -0600

    Merged unit tests for GetNewKanjiDecks() into one.

--- AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/KanjiDeckServiceTests.cs
+++ AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/KanjiDeckServiceTests.cs
@@ -35,8 +35,11 @@ namespace Tests.ApplicationLayer.Services
 		}
 
 		[Theory]
+		//Test case: Deck ids found
 		[InlineData("empty_newKanji_deck.anki2", new long[] { 1707160682667 })]
 		[InlineData("empty_kanjiResource_newKanji_decks.anki2", new long[] { 1707160682667 })]
+		//Test case: Deck ids not found
+		[InlineData("empty_random_decks.anki2", new long[0])]
 		public void Get_new_kanji_decks(string anki2File, long[] expectedDeckIds)
 		{
 			//Arange
@@ -49,22 +52,6 @@ namespace Tests.ApplicationLayer.Services
 
 			//Assert
 			taggedDecks.Select(d => d.Id).Should().BeEquivalentTo(expectedDeckIds);
-		}
-
-		[Theory]
-		[InlineData("empty_random_decks.anki2", new long[0])]
-		public void No_new_kanji_decks_is_empty(string anki2File, long[] expectedDeckIds)
-		{
-			//Arange
-			Anki2Context anki2Context = new Anki2Context(_anki2FolderPath + anki2File);
-			DeckService deckService = new DeckService(new DeckRepository(anki2Context));
-			KanjiDeckService kanjiDeckService = new KanjiDeckService(deckService);
-
-			//Act
-			var learningKanjiDecks = kanjiDeckService.GetNewKanjiDecks();
-
-			//Assert
-			learningKanjiDecks.Select(d => d.Id).Should().BeEquivalentTo(expectedDeckIds);
 		}
 
 		[Theory]
