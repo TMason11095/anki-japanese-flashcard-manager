47362b23bbca8851f281eaa0ceaa00435745261d
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-02 18:36:01 -0600

    Merged unit tests for GetResourceKanjiDecks() into one.

--- AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/KanjiDeckServiceTests.cs
+++ AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/KanjiDeckServiceTests.cs
@@ -15,25 +15,12 @@ namespace Tests.ApplicationLayer.Services
 		string _anki2FolderPath = "./Resources/Anki2 Files/";
 
 		[Theory]
+		//Test case: Deck ids found
 		[InlineData("empty_kanjiResource_deck.anki2", new long[] { 1706982246215 })]
 		[InlineData("empty_kanjiResource_newKanji_decks.anki2", new long[] { 1707160947123 })]
-		public void Get_kanji_resource_decks(string anki2File, long[] expectedDeckIds)
-		{
-			//Arange
-			Anki2Context anki2Context = new Anki2Context(_anki2FolderPath + anki2File);
-			DeckService deckService = new DeckService(new DeckRepository(anki2Context));
-			KanjiDeckService kanjiDeckService = new KanjiDeckService(deckService);
-
-			//Act
-			var taggedDecks = kanjiDeckService.GetResourceKanjiDecks();
-
-			//Assert
-			taggedDecks.Select(d => d.Id).Should().BeEquivalentTo(expectedDeckIds);
-		}
-
-		[Theory]
+		//Test case: Deck ids not found
 		[InlineData("empty_random_decks.anki2", new long[0])]
-		public void No_kanji_resource_decks_is_empty(string anki2File, long[] expectedDeckIds)
+		public void Get_kanji_resource_decks(string anki2File, long[] expectedDeckIds)
 		{
 			//Arange
 			Anki2Context anki2Context = new Anki2Context(_anki2FolderPath + anki2File);
