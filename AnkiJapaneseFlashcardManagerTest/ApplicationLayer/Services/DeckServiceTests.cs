0472d2cb819832c895a1628b845cb41f6f352f1e
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-02 18:58:41 -0600

    Merged unit tests for GetTaggedDecks() into one.

--- AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/DeckServiceTests.cs
+++ AnkiJapaneseFlashcardManagerTest/ApplicationLayer/Services/DeckServiceTests.cs
@@ -15,27 +15,15 @@ namespace Tests.ApplicationLayer.Services
 		string _anki2FolderPath = "./Resources/Anki2 Files/";
 
 		[Theory]
+		//Test case: Deck ids found
 		[InlineData("empty_kanjiResource_deck.anki2", "KanjiResource", new long[] { 1706982246215 })]
 		[InlineData("empty_kanjiResource_newKanji_decks.anki2", "KanjiResource", new long[] { 1707160947123 })]
 		[InlineData("empty_random_decks.anki2", "Random", new long[] { 1706982351536, 1706982318565 })]
 		[InlineData("empty_random_decks.anki2", "RandomResource", new long[] { 1706982318565 })]
-		public void Get_decks_by_description_tag(string anki2File, string deckTagName, long[] expectedDeckIds)
-		{
-			//Arange
-			Anki2Context anki2Context = new Anki2Context(_anki2FolderPath + anki2File);
-			DeckService deckService = new DeckService(new DeckRepository(anki2Context));
-
-			//Act
-			var taggedDecks = deckService.GetTaggedDecks(deckTagName);
-
-			//Assert
-			taggedDecks.Select(d => d.Id).Should().BeEquivalentTo(expectedDeckIds);
-		}
-
-		[Theory]
+		//Test case: Deck ids not found
 		[InlineData("empty_kanjiResource_deck.anki2", "NonExistentTag", new long[0])]
 		[InlineData("empty_random_decks.anki2", "KanjiResource", new long[0])]
-		public void No_decks_with_matching_description_tag_is_empty(string anki2File, string deckTagName, long[] expectedDeckIds)
+		public void Get_decks_by_description_tag(string anki2File, string deckTagName, long[] expectedDeckIds)
 		{
 			//Arange
 			Anki2Context anki2Context = new Anki2Context(_anki2FolderPath + anki2File);
