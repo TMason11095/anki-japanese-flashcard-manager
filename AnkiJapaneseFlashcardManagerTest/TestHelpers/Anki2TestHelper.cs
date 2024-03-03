9b9cb8b550806316c1f072022d9547eafbfc10e0
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-02 23:36:31 -0600

    Created Anki2TestHelper.

---
+++ AnkiJapaneseFlashcardManagerTest/TestHelpers/Anki2TestHelper.cs
@@ -0,0 +1,45 @@
+﻿using AnkiJapaneseFlashcardManager.DataAccessLayer.Contexts;
+using AnkiJapaneseFlashcardManager.DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TestHelpers
+{
+	public class Anki2TestHelper
+	{
+		private const string Anki2FolderPath = "./Resources/Anki2 Files/";
+		private string _anki2File;
+		private string _anki2FilePath;
+		private Anki2Context _anki2Context;
+		private DeckRepository _deckRepository;
+
+		public Anki2TestHelper(string anki2File)//, bool createEditableCopy = false)
+		{
+			//Setup the file path
+			_anki2File = anki2File;
+			_anki2FilePath = Anki2FolderPath + _anki2File;
+
+			////Create the editable copy
+			//if (createEditableCopy)
+			//{
+
+			//}
+
+			//Setup the DbContext
+			_anki2Context = new Anki2Context(_anki2FilePath);
+		}
+
+		public DeckRepository GetDeckRepository()
+		{
+			if (_deckRepository is null)
+			{
+				_deckRepository = new DeckRepository(_anki2Context);
+			}
+			//Return
+			return _deckRepository;
+		}
+	}
+}
