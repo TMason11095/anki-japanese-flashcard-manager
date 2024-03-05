bc13129771b3fae6d2a34ba1b6d6f237c3162b6d
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-05 15:07:43 -0600

    Created Anki2Extension.GetIds().

---
+++ AnkiSentenceCardBuilder/ApplicationLayer/Helpers/Anki2Extension.cs
@@ -0,0 +1,22 @@
+﻿using AnkiJapaneseFlashcardManager.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnkiJapaneseFlashcardManager.ApplicationLayer.Helpers
+{
+	public static class Anki2Extension
+	{
+		public static IEnumerable<long> GetIds(this IEnumerable<Note> notes)
+		{
+			return notes.Select(n => n.Id);
+		}
+
+		public static IEnumerable<long> GetIds(this IEnumerable<Deck> decks)
+		{
+			return decks.Select(d => d.Id);
+		}
+	}
+}
