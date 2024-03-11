0ce892765ab62d95f2fe319b217e75b9ae28e4bb
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-10 20:50:38 -0500

    Refactored IKanjiNoteService to only include the required functions.

--- AnkiSentenceCardBuilder/ApplicationLayer/Interfaces/Services/IKanjiNoteService.cs
+++ AnkiSentenceCardBuilder/ApplicationLayer/Interfaces/Services/IKanjiNoteService.cs
@@ -9,10 +9,6 @@ namespace AnkiJapaneseFlashcardManager.ApplicationLayer.Interfaces.Services
 {
     public interface IKanjiNoteService
     {
-        IEnumerable<Note> GetKanjiNotes(IEnumerable<Note> deckNotes);
-        IEnumerable<string> GetSubKanjiIds(IEnumerable<Note> kanjiNotes);
         List<Note> PullAllSubKanjiNotesFromNoteList(ref List<Note> noteList, List<Note> originalKanjiNotes);
-        IEnumerable<Note> GetTaggedNotes(IEnumerable<Note> deckNotes, string noteTagName);
-        IEnumerable<Note> GetNotesByKanjiIds(IEnumerable<Note> kanjiNotes, IEnumerable<string> kanjiIds);
     }
 }
