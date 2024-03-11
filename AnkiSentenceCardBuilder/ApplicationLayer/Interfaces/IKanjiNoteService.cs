ca9ff83bd34f7421f9ef61c54c7868fe0a5c7c84
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-10 20:18:34 -0500

    Created and implemented IKanjiNoteService.

---
+++ AnkiSentenceCardBuilder/ApplicationLayer/Interfaces/IKanjiNoteService.cs
@@ -0,0 +1,18 @@
+﻿using AnkiJapaneseFlashcardManager.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnkiJapaneseFlashcardManager.ApplicationLayer.Interfaces
+{
+	public interface IKanjiNoteService
+	{
+		IEnumerable<Note> GetKanjiNotes(IEnumerable<Note> deckNotes);
+		IEnumerable<string> GetSubKanjiIds(IEnumerable<Note> kanjiNotes);
+		List<Note> PullAllSubKanjiNotesFromNoteList(ref List<Note> noteList, List<Note> originalKanjiNotes);
+		IEnumerable<Note> GetTaggedNotes(IEnumerable<Note> deckNotes, string noteTagName);
+		IEnumerable<Note> GetNotesByKanjiIds(IEnumerable<Note> kanjiNotes, IEnumerable<string> kanjiIds);
+	}
+}
