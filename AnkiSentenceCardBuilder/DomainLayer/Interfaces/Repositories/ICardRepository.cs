48bfd0a9d81ffacb4b7ab9ee16bf74c0c58224c5
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-10 19:35:09 -0500

    Created ICardRepository interface.

---
+++ AnkiSentenceCardBuilder/DomainLayer/Interfaces/Repositories/ICardRepository.cs
@@ -0,0 +1,16 @@
+﻿using AnkiJapaneseFlashcardManager.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnkiJapaneseFlashcardManager.DomainLayer.Interfaces.Repositories
+{
+	public interface ICardRepository
+	{
+		IEnumerable<Note> GetDeckNotes(long deckId);
+		bool MoveNotesBetweenDecks(IEnumerable<long> noteIds, long newDeckId);
+		IEnumerable<long> GetNoteIdsWithAtLeastInterval(IEnumerable<long> noteIds, int interval);
+	}
+}
